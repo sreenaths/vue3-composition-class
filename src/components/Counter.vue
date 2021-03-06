2282f30865d51b2fc2b73bb90a9a863cc318b16e
sreenaths <somarajapuram@gmail.com>
2021-03-06 19:49:29 +0530

    Improving the example to not use bind

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,30 +1,40 @@
+This is how a component could look.
+
 <template>
   <div class="counter">
     Counter: {{ valueInternal }}<br />
-    <button type="button" @click="add">Add</button>&nbsp;
-    <button type="button" @click="subtract">Sub</button>
+    <button type="button" @click="onAdd">Add</button>&nbsp;
+    <button type="button" @click="onSubtract">Sub</button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, Ref } from "vue";
+import { defineComponent, ref } from "vue";
 
 class Counter {
-  valueInternal: Ref<number>;
+  valueInternal: any;
 
   constructor(val: number) {
-    this.valueInternal = ref(val);
+    this.valueInternal = ref<number>(val);
 
-    this.add = this.add.bind(this);
-    this.subtract = this.subtract.bind(this);
+    // Guess this is needed because of hasOwn check @ https://github.com/vuejs/vue-next/blob/master/packages/runtime-core/src/componentPublicInstance.ts#L293
+    // We must be able to remove it with official support
+    Object.assign(this, {
+      onAdd: this.onAdd,
+      onSubtract: this.onSubtract
+    });
   }
 
-  add() {
-    this.valueInternal.value++;
+  onAdd() {
+    this.valueInternal++;
   }
 
-  subtract() {
-    this.valueInternal.value--;
+  onSubtract() {
+    this.valueInternal--;
+  }
+
+  someOtherMethod() {
+    // Statements
   }
 }
 
