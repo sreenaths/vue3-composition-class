c9cbde961b18ef31eb4e06f67b12ff91dcc0b75a
sreenaths <somarajapuram@gmail.com>
2021-03-07 11:42:37 +0530

    Adding creation time to perf test

--- tests/ClassTest.js
+++ tests/ClassTest.js
@@ -1,4 +1,5 @@
 const mem1 = process.memoryUsage();
+const time1 = process.hrtime.bigint();
 
 class Counter {
   valueInternal;
@@ -19,11 +20,33 @@ class Counter {
   onSubtract() {
     this.valueInternal--;
   }
+
+  someOtherFunction1() {
+    console.log(this.valueInternal);
+  }
+  someOtherFunction2() {
+    console.log(this.valueInternal);
+  }
+  someOtherFunction3() {
+    console.log(this.valueInternal);
+  }
+  someOtherFunction4() {
+    console.log(this.valueInternal);
+  }
+  someOtherFunction5() {
+    console.log(this.valueInternal);
+  }
+  someOtherFunction6() {
+    console.log(this.valueInternal);
+  }
 }
 
 const getInstance2 = props => new Counter(props);
 
 const instances = new Array(1000).fill({}).map(getInstance2);
+
+const time2 = process.hrtime.bigint();
 const mem2 = process.memoryUsage();
 
-console.log(`Memory Used: ${mem2.heapUsed - mem1.heapUsed} bytes`); // Memory Used: 127248 bytes
+console.log(`Memory Used: ${mem2.heapUsed - mem1.heapUsed} bytes`); // Memory Used: 128528 bytes
+console.log(`Time taken: ${time2 - time1} nanoseconds`); // Time taken: 586271 nanoseconds
--- tests/ClosureTest.js
+++ tests/ClosureTest.js
@@ -1,4 +1,5 @@
 const mem1 = process.memoryUsage();
+const time1 = process.hrtime.bigint();
 
 const getInstance = props => {
   const valueInternal = props.value;
@@ -11,11 +12,33 @@ const getInstance = props => {
     },
     onSubtract() {
       valueInternal.value--;
+    },
+
+    someOtherFunction1() {
+      console.log(valueInternal.value);
+    },
+    someOtherFunction2() {
+      console.log(valueInternal.value);
+    },
+    someOtherFunction3() {
+      console.log(valueInternal.value);
+    },
+    someOtherFunction4() {
+      console.log(valueInternal.value);
+    },
+    someOtherFunction5() {
+      console.log(valueInternal.value);
+    },
+    someOtherFunction6() {
+      console.log(valueInternal.value);
     }
   };
 };
 
 const instances = new Array(1000).fill({}).map(getInstance);
+
+const time2 = process.hrtime.bigint();
 const mem2 = process.memoryUsage();
 
-console.log(`Memory Used: ${mem2.heapUsed - mem1.heapUsed} bytes`); //Memory Used: 239656 bytes
+console.log(`Memory Used: ${mem2.heapUsed - mem1.heapUsed} bytes`); // Memory Used: 532792 bytes
+console.log(`Time taken: ${time2 - time1} nanoseconds`); // Time taken: 1573748 nanoseconds
